19d7d76de47769cd3ffafc39eb816fe5bee1ef53
Alexandra Averyanova <alexaver11801@gmail.com>
2023-05-13 22:51:58 +0300

    add controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private GameObject camera;
+    [SerializeField] private float movementSpeed;
+    [SerializeField] private float rotationSpeed;
+
+    private void Update()
+    {
+        var moveDirection = (transform.right * Input.GetAxis("Horizontal") 
+                             + transform.forward * Input.GetAxis("Vertical")) * movementSpeed;
+        transform.position = Vector3.Lerp(transform.position, transform.position + moveDirection, Time.deltaTime);
+        var rotateDirection = transform.up * Input.GetAxis("Mouse X") * rotationSpeed;
+        transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, transform.eulerAngles + rotateDirection, Time.deltaTime);
+        var cameraRotateDirection = Vector3.left * Input.GetAxis("Mouse Y") * rotationSpeed;
+        camera.transform.localEulerAngles = Vector3.Lerp(camera.transform.localEulerAngles,
+            camera.transform.localEulerAngles + cameraRotateDirection, Time.deltaTime);
+    }
+}

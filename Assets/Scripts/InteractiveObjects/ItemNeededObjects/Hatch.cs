0ad07b98978a4c7b8059d84a475416f1c6779299
Alexandra Averyanova <alexaver11801@gmail.com>
2023-05-27 19:12:13 +0300

    add hatch

---
+++ Assets/Scripts/InteractiveObjects/ItemNeededObjects/Hatch.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace InteractiveObjects.ItemNeededObjects
+{
+    public class Hatch : ItemNeededInteractiveObject
+    {
+        [SerializeField] private GameObject transportPoint;
+        
+        protected override void Interact(PlayerController player)
+        {
+            base.Interact(player);
+            transportPoint.SetActive(true);
+        }
+    }
+}
